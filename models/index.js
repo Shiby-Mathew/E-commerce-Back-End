ada6bb81c9abea9e04447e25c7b632c328650725
Shiby-Mathew <shiby.ajay@gmail.com>
2023-05-14 19:48:56 +1000

    Delete is working now

--- models/index.js
+++ models/index.js
@@ -8,20 +8,28 @@ const ProductTag = require("./ProductTag");
 
 Product.belongsTo(Category, {
   foreignKey: "category_id",
-  // onDelete: "CASCADE",
+  onDelete: "SET NULL",
 });
 // Categories have many Products
 
 Category.hasMany(Product, {
   foreignKey: "category_id",
-  onDelete: "CASCADE",
+  //onDelete: "SET NULL",
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: "ProductTag", foreignKey: "product_id" });
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+  onDelete: "SET NULL",
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: "ProductTag", foreignKey: "tag_id" });
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+  onDelete: "SET NULL",
+});
 
 module.exports = {
   Product,
